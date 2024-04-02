b68d065d12945bfddf080aba1e1ad6681a3896c4
Tricide <cameronjspears@icloud.com>
2024-04-02 12:37:34 -0400

    Fixed so far, new file not working

---
+++ Views/navigation views/new_budget_screen.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace StellarMVVM_2024.Views;
+
+public partial class new_budget_screen : UserControl
+{
+    public new_budget_screen()
+    {
+        InitializeComponent();
+    }
+}
