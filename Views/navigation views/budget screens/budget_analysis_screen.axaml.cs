d7b4e99c3bd579a9a28b27151e60a90b6637c624
Tricide <cameronjspears@icloud.com>
2024-04-04 15:33:49 -0400

    formulating more

---
+++ Views/navigation views/budget screens/budget_analysis_screen.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StellarMVVM_2024;
+
+public partial class UserControl1 : UserControl
+{
+    public UserControl1()
+    {
+        InitializeComponent();
+    }
+}
--- Views/navigation views/budget screens/new_budget_screen.axaml.cs
+++ Views/navigation views/budget screens/new_budget_screen.axaml.cs
@@ -1,9 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
-using System;
 
 namespace StellarMVVM_2024.Views;
 
@@ -14,8 +11,5 @@ public partial class new_budget_screen : UserControl
         InitializeComponent();
     }
 
-    private void budgetSubmitClicked(object sender, EventArgs e)
-    {
 
-    }
 }
