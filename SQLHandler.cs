1803448beeebb050e545be97807f2fa128f1901f
Tricide <cameronjspears@icloud.com>
2024-04-10 00:00:48 -0400

    I'm going to lose it if I don't sleep

--- SQLHandler.cs
+++ SQLHandler.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace StellarMVVM_2024
 {
     internal class SQLHandler
     {
+        SQLiteConnection sqlite_conn;
         public SQLHandler() {
-            
+            sqlite_conn = CreateConnection();
+        }
+
+        private SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_connection;
+            sqlite_connection = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return sqlite_connection;
+        }
+
+        private void Close()
+        {
+
         }
     }
 }
